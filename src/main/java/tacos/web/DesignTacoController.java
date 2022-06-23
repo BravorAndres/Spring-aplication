12f35b198e7a0160931beb6c5296fbe16a009487
Andres <bravor.andres24@gmail.com>
2022-06-23 10:44:24 -0500

    Archivos designController modificado

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -25,61 +25,76 @@ import org.springframework.validation.Errors;
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
-    
-    @ModelAttribute
-    public void addIngredientsToModel(Model model){
-        List<Ingredient> ingredientes = Arrays.asList(
-            new Ingredient("FLTO","Flour Tortilla",Type.WRAP),
-            new Ingredient("COTO","Corn Tortilla",Type.WRAP),
-            new Ingredient("GRBF","Grounf Beef",Type.PROTEN),
-            new Ingredient("CARN","Carnitas",Type.PROTEN),
-            new Ingredient("TMTO","Diced tomatos",Type.VEGGIES),
-            new Ingredient("LETC","Lettuce",Type.VEGGIES),
-            new Ingredient("CHED","Cheddar",Type.CHEESE),
-            new Ingredient("JACK","Monterrey Jack",Type.CHEESE),
-            new Ingredient("SLSA","Salsa",Type.SAUCE),
-            new Ingredient("SRCR","Sour Cream",Type.SAUCE) 
-            
-        );
 
-        Type[] types = Ingredient.Type.values();
-        for (Type type: types){
-            model.addAttribute(type.toString().toLowerCase(),filterByType(ingredientes,type));
-        }
+@ModelAttribute
+public void addIngredientsToModel(Model model) {
+	List<Ingredient> ingredients = Arrays.asList(
+	  new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+	  new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+	  new Ingredient("GRBF", "Ground Beef", Type.PROTEN),
+	  new Ingredient("CARN", "Carnitas", Type.PROTEN),
+	  new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+	  new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+	  new Ingredient("CHED", "Cheddar", Type.CHEESE),
+	  new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+	  new Ingredient("SLSA", "Salsa", Type.SAUCE),
+	  new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+	);
+
+	Type[] types = Ingredient.Type.values();
+	for (Type type : types) {
+	  model.addAttribute(type.toString().toLowerCase(),
+	      filterByType(ingredients, type));
+	}
+  }
+
+  @ModelAttribute(name = "tacoOrder")
+  public TacoOrder order() {
+    return new TacoOrder();
+  }
+
+  @ModelAttribute(name = "taco")
+  public Taco taco() {
+    return new Taco();
+  }
+
+  @GetMapping
+  public String showDesignForm() {
+    return "design";
+  }
+
+/*
+  @PostMapping
+  public String processTaco(Taco taco,
+  			@ModelAttribute TacoOrder tacoOrder) {
+    tacoOrder.addTaco(taco);
+    log.info("Processing taco: {}", taco);
+
+    return "redirect:/orders/current";
+  }
+ */
+
+  @PostMapping
+  public String processTaco(
+		  @Valid Taco taco, Errors errors,
+		  @ModelAttribute TacoOrder tacoOrder) {
+
+    if (errors.hasErrors()) {
+      return "design";
     }
 
-    @ModelAttribute(name = "tacoOrder")
-    public TacoOrder order(){
-        return new TacoOrder();
-    }
+    tacoOrder.addTaco(taco);
+    log.info("Processing taco: {}", taco);
 
-    @ModelAttribute(name = "taco")
-    public Taco taco(){
-        return new Taco();
-    }
+    return "redirect:/orders/current";
+  }
 
-    @GetMapping
-    public String showDesingForm(){
-        return "design";
-    }
+  private Iterable<Ingredient> filterByType(
+      List<Ingredient> ingredients, Type type) {
+    return ingredients
+              .stream()
+              .filter(x -> x.getType().equals(type))
+              .collect(Collectors.toList());
+  }
 
-
-    @PostMapping
-    public String processTaco(
-            @Valid Taco taco, Errors errors,
-            @ModelAttribute TacoOrder tacoOrder) {
-  
-      if (errors.hasErrors()) {
-        return "design";
-      }
-  
-      tacoOrder.addTaco(taco);
-      log.info("Processing taco: {}", taco);
-  
-      return "redirect:/orders/current";
-    }
-
-    private Iterable<Ingredient> filterByType(List<Ingredient>ingredientes,Type type){
-        return ingredientes.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
-    }
 }
